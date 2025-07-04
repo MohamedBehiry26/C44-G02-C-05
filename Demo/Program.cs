0301b6db19bd0a8ae616e752048dabb9165ec443
Mohamed Mahmoud AbdElsalam Behiry <mmbehiry@gmail.com>
2025-07-04 10:15:43 +0300

    video 2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -10,7 +10,7 @@
             marks[1] = new int[] { 88, 92 }; // Marks for student 2
             marks[2] = new int[] { 80 }; // Marks for student 3
             int[][] marks2 = [new int[3] { 10, 20, 30 }, new int[2] { 10, 20 }, new int[1] { 10 }];
-            for (int i = 0; i < marks.Length; i++)
+            for (int i = 0; i < marks.Length; i++ )
 
             {
                 for (int j = 0; j < marks[i].Length; j++)
