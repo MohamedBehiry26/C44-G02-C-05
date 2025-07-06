1e8481ef474942309c61f54213d9f574c5141f0b
Mohamed Mahmoud AbdElsalam Behiry <mmbehiry@gmail.com>
2025-07-06 06:12:33 +0300

    Q1 & Q2 & Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1.Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
+             Passing by Value(Default for Value Types)
+                What happens?
+A copy of the variable is passed to the method.
+
+Effect:
+            Changes made inside the method do not affect the original variable.
+
+🔹 Passing by Reference(using ref or out)
+What happens?
+A reference to the original variable is passed to the method.
+
+Effect:
+            Changes made inside the method do affect the original variable.
+
+
+            #endregion
         }
     }
 }
